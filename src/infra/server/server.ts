90d34a8e1c301512d752bb3c0e87b5993a8b6af5
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-03 18:21:47 -0300

    alterações no servidor

--- src/infra/server/server.ts
+++ src/infra/server/server.ts
@@ -5,16 +5,18 @@ import { connectToMongo } from '../../infra/database/MongoConnection';
 const app = express();
 app.use(express.json()); 
 
-app.use('/books', bookRoutes); 
-
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3000; 
 const URI = process.env.MONGO_URL;
 
 if(!URI) {
-    throw new Error('Erro ao conectar ao banco de dados');
+  throw new Error('Erro ao conectar ao banco de dados');
 };
-
-
 connectToMongo(URI); 
+
+app.use('/books', bookRoutes); 
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
 
 export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import express from 'express';
 import app from './infra/server/server';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
-

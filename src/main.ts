97412cf84744f8e7d5814a51326b00067db4b592
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-06 17:11:21 -0300

    fix: port e testes de integração

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { config } from '../src/config/environment';
 const PORT = config.port;
 
 app.listen(PORT, () => {
-  console.log('Servidor rodando em http://localhost:',`${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 

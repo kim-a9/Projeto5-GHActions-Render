3be9c73ad8e41e2c85765b263f326abf8a54e331
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-05 21:43:01 -0300

    fix: alterações package para deploy

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,7 +1,11 @@
+import dotenv from'dotenv';
 
+if (process.env.NODE_ENV !== "production"){
+    dotenv.config()
+}
 export const config = {
-    port: process.env.PORT || 'default',
-    mongoUrl: process.env.MONGO_URL || 'default',
-    jwtSecret: process.env.JWT_SECRET || 'default',
-    nodeEnv: process.env.NODE_ENV || 'default'
+    port: process.env.PORT || 3005,
+    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://kimberlysantosalves:UwgrXDwADfCno1b3@aprofunda-render.jmpjac5.mongodb.net/?retryWrites=true&w=majority&appName=Aprofunda-Render',
+    jwtSecret: process.env.JWT_SECRET || 'aprofunda_projeto5_ghactions_render',
+    nodeEnv: process.env.NODE_ENV || 'development'
 }

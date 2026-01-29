6f126909fd34210a441b6e2e7cd2f114c9838b54
kim-a9 <kimberlysantosalves@gmail.com>
2026-01-29 16:43:51 -0300

    refactor: preparando para integração com interface

--- backend/src/infra/server/server.ts
+++ backend/src/infra/server/server.ts
@@ -1,21 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 import  { config }  from '../../config/environment';
 import { bookRoutes } from '../../app/routes/BookRoutes';
 import { connectToMongo } from '../../infra/database/MongoConnection';
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
- const URI = config.mongoUrl;
+app.use('/', bookRoutes); 
+
+const PORT = config.port;
+const URI = config.mongoUrl;
 
 if(!URI) {
   throw new Error('Erro ao conectar ao banco de dados');
 };
 connectToMongo(URI); 
 
-app.use('/', bookRoutes); 
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
 
 
-
-export default app
+export default app;
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import app from './infra/server/server';
 import { config } from '../src/config/environment';
 
-const PORT = config.port || 3000;
+const PORT = config.port;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
